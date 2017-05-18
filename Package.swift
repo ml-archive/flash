c18da43e42cfe842e7153ece268c4581dd91a510
Steffen D. Sommer <stso@nodes.dk>
2017-05-18 21:53:13 +0100

    Update for Vapor 2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Flash",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", Version(2,0,0, prereleaseIdentifiers: ["beta"])),
-        .Package(url: "https://github.com/vapor/auth-provider.git", Version(1,0,0, prereleaseIdentifiers: ["beta"]))
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1)
     ]
 )
