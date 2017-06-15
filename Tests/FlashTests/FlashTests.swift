be9c66e89147f669ff31562752f41180e3710e67
Steffen D. Sommer <stso@nodes.dk>
2017-06-15 16:38:06 +0200

    Removing unused tests

--- Tests/FlashTests/FlashTests.swift
+++ Tests/FlashTests/FlashTests.swift
@@ -3,46 +3,13 @@ import HTTP
 @testable import Flash
 
 class FlashTests: XCTestCase {
-    func testMiddleware() throws {
-        let _ = FlashMiddleware()
-        
-        let _ = try Request(method: .get, uri: "uri")
-        let _ = TestResponse()
-        
-        // Need a session middleware
-        //let _ = try flashMiddleware.respond(to: request, chainingTo: responder)
-        
+    func testExample() throws {
         XCTAssertTrue(true)
-    }
-    
-    func testHelper() throws {
-        do {
-            let request = try Request(method: .get, uri: "uri")
-            let _ = Helper(request: request)
-            
-            // Need a session middleware
-            //try helper.add("custom", "message")
-            
-            
-            XCTAssertTrue(true)
-        } catch {
-            print(error)
-            
-            XCTAssertTrue(false)
-        }
     }
 
     static var allTests : [(String, (FlashTests) -> () throws -> Void)] {
         return [
-            ("testMiddleware", testMiddleware),
-            ("testHelper", testHelper),
-            
+            ("testExample", testExample),
         ]
-    }
-}
-
-class TestResponse: Responder {
-    func respond(to request: Request) throws -> Response {
-        return Response()
     }
 }
