1a62c2897bd4f97e1c812b301c8cf7e937e231bc
Brett R. Toomey <brto@nodes.dk>
2018-04-10 17:41:36 +0200

    Setup Swift library

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Flash",
+    products: [
+        .library(name: "Flash", targets: ["Flash"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
+    ],
+    targets: [
+        .target(name: "Flash", dependencies: ["Vapor"]),
+    ]
+)
