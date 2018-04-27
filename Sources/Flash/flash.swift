d2c71dd7e272ffe567ac4434311025e330b74044
Brett R. Toomey <brto@nodes.dk>
2018-04-27 14:05:37 +0200

    Flash middleware

--- Sources/Flash/flash.swift
+++ Sources/Flash/flash.swift
@@ -2,6 +2,8 @@ import Service
 import Vapor
 
 public final class FlashProvider: Provider {
+    public init() {}
+    
     public func register(_ services: inout Services) throws {
         services.register { container in
             return FlashContainer()
@@ -13,23 +15,83 @@ public final class FlashProvider: Provider {
     }
 }
 
-public final class FlashContainer: Service {
-    public var flashes: [(type: FlashType, message: String)] = []
+public final class Flash: Codable {
+    public var type: FlashType
+    public var message: String
+
+    public init(type: FlashType, message: String) {
+        self.type = type
+        self.message = message
+    }
+
+    public init(_ type: FlashType, _ message: String) {
+        self.type = type
+        self.message = message
+    }
 }
 
-public enum FlashType: String {
-    case error = "error"
-    case success = "success"
-    case info = "info"
-    case warning = "warning"
+public final class FlashContainer: Codable, Service {
+    public var new: [Flash] = []
+    public var old: [Flash] = []
+
+    public var flashes: [Flash] {
+        get {
+            return new
+        }
+
+        set {
+            new = newValue
+        }
+    }
+}
+
+public enum FlashType: String, Codable {
+    case error
+    case success
+    case info
+    case warning
 }
 
 extension Response {
     public func flash(_ type: FlashType, _ message: String) -> Response {
         if let container = try? privateContainer.make(FlashContainer.self) {
-            container.flashes.append((type, message))
+            container.flashes.append(.init(type, message))
         }
 
         return self
+    }
+}
+
+public struct FlashMiddleware: Middleware {
+    public init() {}
+    
+    /// See Middleware.respond
+    public func respond(to req: Request, chainingTo next: Responder) throws -> Future<Response> {
+        try FlashMiddleware.handle(req: req)
+        return try next.respond(to: req)
+            .map(to: Response.self) { resp in
+                try FlashMiddleware.handle(req: req, resp: resp)
+                return resp
+            }
+    }
+
+    public static func handle(req: Request) throws {
+        let session = try req.session()
+
+        if let data = session["_flash"]?.data(using: .utf8) {
+            let flash = try JSONDecoder().decode(FlashContainer.self, from: data)
+            let container = try req.privateContainer.make(FlashContainer.self)
+            container.new = flash.new
+            container.old = flash.old
+        }
+    }
+
+    public static func handle(req: Request, resp: Response) throws {
+        let container = try resp.privateContainer.make(FlashContainer.self)
+        let flash = try String(
+            data: JSONEncoder().encode(container),
+            encoding: .utf8
+        )
+        try req.session()["_flash"] = flash
     }
 }
