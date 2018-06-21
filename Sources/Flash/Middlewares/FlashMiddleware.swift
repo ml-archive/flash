1c677321eac5b56583f804428b3600a050bc0e08
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-06-21 09:48:12 +0200

    Don't use private container. Needs to be tested.
    - API docs

--- Sources/Flash/Middlewares/FlashMiddleware.swift
+++ Sources/Flash/Middlewares/FlashMiddleware.swift
@@ -1,41 +1,49 @@
 import Vapor
 
+/// Middleware that:
+/// - decodes flashes from the `Session` and puts them on the `Request`s container.
+/// - encodes flashes from the `Response` to the `Session`.
 public struct FlashMiddleware: Middleware, ServiceType {
-    private static let sessionKey = "_flash"
-
+    /// See `ServiceType.makeService`.
     public static func makeService(for container: Container) throws -> FlashMiddleware {
         return .init()
     }
 
+    /// Create a new `FlashMiddleware`.
     public init() {}
 
     /// See Middleware.respond
     public func respond(to req: Request, chainingTo next: Responder) throws -> Future<Response> {
-        try FlashMiddleware.handle(req: req)
-        return try next.respond(to: req)
-            .map(to: Response.self) { resp in
-                try FlashMiddleware.handle(req: req, resp: resp)
-                return resp
+        try req.decodeFlashDataFromSession()
+        return try next
+            .respond(to: req)
+            .try { resp in
+                try req.encodeFlashDataToSession(from: resp)
             }
     }
+}
 
-    public static func handle(req: Request) throws {
-        let session = try req.session()
+private let sessionKey = "_flash"
 
-        if let data = session[sessionKey]?.data(using: .utf8) {
-            let flash = try JSONDecoder().decode(FlashContainer.self, from: data)
-            let container = try req.privateContainer.make(FlashContainer.self)
-            container.new = flash.new
-            container.old = flash.old
+extension Request {
+    func decodeFlashDataFromSession() throws {
+        let container = try privateContainer.make(FlashContainer.self)
+
+        guard let data = try session()[sessionKey]?.data(using: .utf8) else {
+            container.flashes = []
+            return
         }
+
+        let decoded = try JSONDecoder().decode(FlashContainer.self, from: data)
+        container.flashes = decoded.flashes
     }
 
-    public static func handle(req: Request, resp: Response) throws {
+    func encodeFlashDataToSession(from resp: Response) throws {
         let container = try resp.privateContainer.make(FlashContainer.self)
         let flash = try String(
             data: JSONEncoder().encode(container),
             encoding: .utf8
         )
-        try req.session()[sessionKey] = flash
+        try session()[sessionKey] = flash
     }
 }
--- Sources/Flash/Models/FlashContainer.swift
+++ Sources/Flash/Models/FlashContainer.swift
@@ -1,16 +1,6 @@
 import Vapor
 
+/// Container that contains all `Flash`es.
 public final class FlashContainer: Codable, Service {
-    public var new: [Flash] = []
-    public var old: [Flash] = []
-
-    public var flashes: [Flash] {
-        get {
-            return new
-        }
-
-        set {
-            new = newValue
-        }
-    }
+    public var flashes: [Flash] = []
 }
