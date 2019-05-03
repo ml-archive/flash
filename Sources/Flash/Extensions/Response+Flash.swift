12df5c8a88b89f23159b516504188521807d97eb
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2019-05-03 23:59:39 +0200

    Remove redundant access modifiers

--- Sources/Flash/Extensions/Response+Flash.swift
+++ Sources/Flash/Extensions/Response+Flash.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 public extension Response {
-    public func flash(_ type: Flash.Kind, _ message: String) -> Response {
+    func flash(_ type: Flash.Kind, _ message: String) -> Response {
         if let container = try? privateContainer.make(FlashContainer.self) {
             container.flashes.append(.init(type, message))
         }
