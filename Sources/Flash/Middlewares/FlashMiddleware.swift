ae789f0a2d76c11eb10afd15416ab29334d3e248
Tony Arnold <tony@thecocoabots.com>
2020-03-06 16:39:21 +1100

    Fix the logic around flash copying in the middleware

--- Sources/Flash/Middlewares/FlashMiddleware.swift
+++ Sources/Flash/Middlewares/FlashMiddleware.swift
@@ -3,20 +3,14 @@ import Vapor
 /// Middleware that passes along any flashes from the session to the next incoming request.
 public struct FlashMiddleware: Middleware {
     public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
-        handle(request: request)
+        // Copy any flashes from the session into the request before responding to it, without overwriting any existing flashes that have been inserted
+        request.flashes += request.session.flashes
 
         return next.respond(to: request).map { response in
-            self.handle(request: request, response: response)
+            // Copy any flashes from the response to this request back into the session, replacing any existing flashes that are there already
+            request.session.flashes = response.flashes
 
             return response
         }
-    }
-
-    private func handle(request: Request) {
-        request.flashes = request.session.flashes
-    }
-
-    private func handle(request: Request, response: Response) {
-        request.session.flashes = request.flashes
     }
 }
