2a5c5e7b87df7e8b8872ccabe4474cb3ee2a56e8
Tony Arnold <tony@thecocoabots.com>
2020-03-12 21:37:33 +1100

    Add a public initializer to the middleware

--- Sources/Flash/Middlewares/FlashMiddleware.swift
+++ Sources/Flash/Middlewares/FlashMiddleware.swift
@@ -2,6 +2,8 @@ import Vapor
 
 /// Middleware that passes along any flashes from the session to the next incoming request.
 public struct FlashMiddleware: Middleware {
+    public init() {}
+    
     public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
         // Copy any flashes from the session into the request before responding to it, without overwriting any existing flashes that have been inserted
         request.flashes += request.session.flashes
