055c8e2402ce7cabc44b79d01a051b27e80d1be7
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2020-04-27 21:30:09 +0200

    Update Vapor

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
         .target(name: "Flash", dependencies: [
